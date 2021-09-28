89e54df61d3a8c3d852405f0f6e3d3d1beb68721
leesangha <sanghalee1@naver.com>
2021-09-28 22:57:20 +0900

    test layout style

---
+++ components/layout.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+
+const Layout: NextPage = () => {
+  return (
+    <nav>
+      <div>Health Jandi</div>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Features</a>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Layout;
